8e866d924b7a8d0ab59b97e6cee595922970fef5
meru rendy wirawan <meru.r.wirawan@alfadigital.id>
2020-02-15 14:45:44 +0700

    200215: fix upload and compress img

--- properties/index.js
+++ properties/index.js
@@ -4,12 +4,12 @@ import appLokalProduction from './app-lokal'
 // eslint-disable-next-line import/no-mutable-exports
 let properties
 
-if (process.env.MODE_ENV === 'production') {
-  console.log('masuk production')
+if (process.env.MODE_ENV === 'lokal') {
+  console.log('masuk lokal')
   properties = appLokalProduction
 } else {
+  console.log('masuk production')
   properties = appProductionProperties
-  console.log('masuk lokal')
 }
 
 export default properties
